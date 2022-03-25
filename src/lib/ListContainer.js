e944fd25fc73e5f9aaa135fa6db03b487d5cbf2b
Karl MARQUES <kmarques@vetixy.com>
2022-03-25 11:10:13 +0100

    TP1

--- src/lib/ListContainer.js
+++ src/lib/ListContainer.js
@@ -1,3 +1,13 @@
+// TP: Faire une gestion de liste en gardant l'aspect technico-générique
+// Modifier ce composant pour que l'on puisse:
+// - Ajouter des éléments à la liste
+// - Supprimer des éléments de la liste
+//// - Récupérer la liste
+// - Modifier les éléments de la liste
+
+// Exemple d'utilisation:
+// - TodoList
+
 export default function ListContainer({ data, ItemComponent }) {
   return data.map((item) => <ItemComponent item={item} />);
 }
